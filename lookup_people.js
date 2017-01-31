96832708435294aef2a4724ca10dfca6f353a68a
Anthony Langford <anthony.langford@gmail.com>
2017-01-31 16:48:36 +0000

    lookup_people.js performs search query from node input on heroku datab database

---
+++ lookup_people.js
@@ -0,0 +1,29 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+const q = process.argv[2];
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT * FROM famous_people WHERE UPPER(first_name) LIKE UPPER($1) OR UPPER(last_name) LIKE UPPER($1)", [`%${q}%`], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    } else {
+      console.log("Searching...");
+      console.log("Found " + result.rows.length + " person(s) by the name '" + result.rows[0].first_name + " " + result.rows[0].last_name + "'");
+      console.log("- " + result.rows[0].id + ": " + result.rows[0].first_name + " " + result.rows[0].last_name + ", born '" + result.rows[0].birthdate.toString().slice(0, 15) + "'");
+    }
+    client.end();
+  });
+});
